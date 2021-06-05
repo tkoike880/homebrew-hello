8b24b600ec3ed83455c09915be5977e3a9ed0219
Takuya Koike <49141283+tkoike880@users.noreply.github.com>
2021-06-06 02:58:00 +0900

    Update test-hello.rb

--- test-hello.rb
+++ test-hello.rb
@@ -1,15 +1,13 @@
 require "formula"
-class Restcli < Formula
-    desc "A missing commandline application for execute IntelliJ HTTP Client files"
-    homepage "https://restcli.github.io/"  
-    url "https://github.com/restcli/restcli/releases/download/v1.7.4/brew_restcli.zip"
+class TestHello < Formula
+    desc "Homebrew Testing Project"
+    homepage ""  
+    url "https://github.com/tkoike880/test-hello/archive/refs/tags/v1.0.0.zip"
     sha256 "7e8d5d7c64c31b3d5b3768a69960d0c11b419bafb6f41c0ac2410ab85b56e1cb"
     license "MIT"
-    version "1.7.4"
+    version "1.0.0"
 
     def install
-        inreplace "brew/restcli", "##PREFIX##", "#{prefix}"
-        prefix.install "restcli.jar"
-        bin.install "brew/restcli"
+        
     end
 end
